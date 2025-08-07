b8166f23f92b45c6cd6e7c388b34a78e876a0bf9
Benjamin McRae <benjamin@supercede.com>
2025-08-07 11:39:38 +0100

    escape backticks

--- Data/FileEmbed/RelativePath.hs
+++ Data/FileEmbed/RelativePath.hs
@@ -32,7 +32,7 @@ import Prelude as P
 --
 -- @$(makeRelativeToProject "data/foo.txt" >>= embedFile)@
 --
--- @$$(makeRelativeToProject "data/foo.txt" `bindCode` Typed.embedFile)@
+-- @$$(makeRelativeToProject "data/foo.txt" \`bindCode\` Typed.embedFile)@
 --
 -- @since 0.0.10
 makeRelativeToProject :: (Quasi m) => FilePath -> m FilePath
